8771f2feac061dbcc8a277b1006af73388ec8114
ArbaazOnGithub <arbaaz@n1solution.com>
2026-04-20 23:17:41 +0530

    Fix 403 Forbidden errors and enhance login form security

--- src/Components/Authorization/Login.jsx
+++ src/Components/Authorization/Login.jsx
@@ -206,6 +206,7 @@ const Login = () => {
               className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
               placeholder="name@company.com"
               required
+              autoComplete="email"
             />
           </div>
           <div>
@@ -219,6 +220,7 @@ const Login = () => {
               className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
               placeholder="••••••••"
               required
+              autoComplete="current-password"
             />
           </div>
           <div className="text-center">
